3138ad0fb7d65047dbec09597cd32b8e7f5465fb
Felipe Moura <felipemimoura@gmail.com>
2024-10-03 07:26:01 -0300

    test: ensure SignUpController return 400 if no name is provided

---
+++ src/presentation/controllers/singup.spec.ts
@@ -0,0 +1,17 @@
+import { SingUpController } from "./singup";
+
+describe("SingUpController", () => {
+  test("Should return 400 if no name is provided", () => {
+    const sut = new SingUpController(); //System under test
+    const httpRequest = {
+      body: {
+        // name: 'any_name',
+        email: "any_mail@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
