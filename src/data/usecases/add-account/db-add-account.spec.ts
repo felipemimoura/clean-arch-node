1da260cc298e3e9aa037c330958f3e16763e7edf
Felipe Moura <felipe.moura@proa.ai>
2024-10-15 20:40:48 -0300

    test: ensure DbAddAccout throws if Encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -15,7 +15,6 @@ const makeEncrypter = (): Encrypter => {
   return new EncrypterStub();
 };
 
-
 const makeSut = (): SutType => {
   const encrypterStub = makeEncrypter();
   const sut = new DbAddAccount(encrypterStub);
@@ -37,5 +36,22 @@ describe("DbAddAccount UseCase", () => {
     await sut.add(accountData);
 
     expect(encryptSpy).toHaveBeenCalledWith("valid_password");
+  });
+
+  test("Should throw if Encrypter throws ", async () => {
+    const { encrypterStub, sut } = makeSut();
+    jest
+      .spyOn(encrypterStub, "encrypt")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+    const promise = sut.add(accountData);
+
+    await expect(promise).rejects.toThrow();
   });
 });
